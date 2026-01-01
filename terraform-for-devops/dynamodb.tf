6e8cf4f4d3323ded59f3458d5f1e17e83945070a
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-01-01 13:48:53 +0530

    Set billing mode to PAY_PER_REQUEST for DynamoDB

--- terraform-for-devops/dynamodb.tf
+++ terraform-for-devops/dynamodb.tf
@@ -1,6 +1,6 @@
 resource "aws_dynamodb_table" "my_app_table" {
     name = "${var.my_enviroment}-test-my-app-table-d"
-    billing_mode = ""
+    billing_mode = "PAY_PER_REQUEST"
     hash_key = "id"
     attribute {
         name = "id"
