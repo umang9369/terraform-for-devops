3ee90322af1f3bb89c83b0583a6bf75986f5000c
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-01-01 13:48:39 +0530

    DynamoDB table configuration

--- terraform-for-devops/dynamodb.tf
+++ terraform-for-devops/dynamodb.tf
@@ -1,6 +1,6 @@
 resource "aws_dynamodb_table" "my_app_table" {
     name = "${var.my_enviroment}-test-my-app-table-d"
-    billing_mode = "PAY_PER_REQUEST"
+    billing_mode = ""
     hash_key = "id"
     attribute {
         name = "id"
