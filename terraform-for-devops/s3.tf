2b6c8603eac5e9e558c3593e7f09e7ec4e363768
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-01-06 23:27:02 +0530

    Delete unused aws_s3_bucket2 resource
    Removed the second S3 bucket resource definition.

--- terraform-for-devops/s3.tf
+++ terraform-for-devops/s3.tf
@@ -4,9 +4,3 @@ resource "aws_s3_bucket" "testbucket" {
             Name = "${var.my_enviroment}-test-my-app-bucket-d"
         }
 }
-resource "aws_s3_bucket2" "prodbucket" {
-    bucket = "${var.my_enviroment}-test-my-app-bucket-d2"
-    tags = {
-            Name = "${var.my_enviroment}-test-my-app-bucket-d2"
-        }
-}
