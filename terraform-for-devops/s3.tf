d021955cad50f50456e37942a309aeab0a847d0a
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-01-06 23:26:41 +0530

    Add second S3 bucket resource for production

--- terraform-for-devops/s3.tf
+++ terraform-for-devops/s3.tf
@@ -4,3 +4,9 @@ resource "aws_s3_bucket" "testbucket" {
             Name = "${var.my_enviroment}-test-my-app-bucket-d"
         }
 }
+resource "aws_s3_bucket2" "prodbucket" {
+    bucket = "${var.my_enviroment}-test-my-app-bucket-d2"
+    tags = {
+            Name = "${var.my_enviroment}-test-my-app-bucket-d2"
+        }
+}
